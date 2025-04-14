5fd48f313cad144572ec583b2c6d8a613f47f9d2
giovaCUDME <giovanni.molinet.navarrete@alumnos.sip.cl>
2025-04-14 10:59:01 -0400

    staged

--- loops/elelefante.py
+++ loops/elelefante.py
@@ -1,7 +1,12 @@
 #giovanni molinet
 #14-04-2025
 
-elefante = int(input("¿cuantos elefantes quieres que se columpien?"))
+num_elefante = int(input("¿cuantos elefantes quieres que cante?:"))
 
-for i in elefante:
-    print("")
+for i in range(1, num_elefante +1):
+    if i == 1:
+        print (f"{i} un elefante se balanceaba se valanceava sobre la tela de una araña.")
+
+    else:
+        print(f"{i} elefante se valanceava sobre la tela de una araña.")
+    print("como veia que resistia, fueron a llamar otro elefante.\n")
