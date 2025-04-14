004d2909a2503c8b3b08d042f4c3380dc037c7d8
giovaCUDME <giovanni.molinet.navarrete@alumnos.sip.cl>
2025-04-14 11:06:26 -0400

    cambios

---
+++ loops/mixtape.py
@@ -0,0 +1,12 @@
+#giovanni molinet
+#14-04-2025
+
+playlist = ['Porches - rangerover',
+            'Mount Eerie - You Swan, Go On',
+            'Carolyn polachek - Look at me Now',
+            'Pinegrove - Darkness',
+            'LVL UP - Spirit Was',
+            'Mitski _ First Love / Late Spring']
+
+for i in playlist:
+    print(i)
