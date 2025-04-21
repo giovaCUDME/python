e3ae570e8d1288a5a4733e60a35e8ff3b449e997
giovaCUDME <giovanni.molinet.navarrete@alumnos.sip.cl>
2025-04-21 12:39:33 -0400

    staged

---
+++ funciones/comentarios.py
@@ -0,0 +1,13 @@
+#giovanni molinet
+#21-04-2025
+# #yo soy megatron
+
+def mi_funcion_suma(a, b):
+    """
+    descripcion de la funcion a realizar
+    """
+    return a+b
+
+mi_funcion_suma(2, 6)
+
+print(mi_funcion_suma(2, 6))
