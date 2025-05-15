70ad6fe8124402ad4b2e391ba2bd57c9848511b1
giovaCUDME <giovanni.molinet.navarrete@alumnos.sip.cl>
2025-05-15 10:19:28 -0400

    staged

--- loops/funcion_intermedia.py
+++ loops/funcion_intermedia.py
@@ -1,16 +1,16 @@
 #giovanni molinet
 
-cantantes = [
+# cantantes = [
 
-   {"nombre": "Ricky Martin", "pais": "Puerto Rico"},
+#    {"nombre": "Ricky Martin", "pais": "Puerto Rico"},
 
-   {"nombre": "Chayanne", "pais": "Puerto Rico"},
+#    {"nombre": "Chayanne", "pais": "Puerto Rico"},
 
-   {"nombre": "José José", "pais": "México"},
+#    {"nombre": "José José", "pais": "México"},
 
-   {"nombre": "Juan Luis Guerra", "pais": "República Dominicana"}
+#    {"nombre": "Juan Luis Guerra", "pais": "República Dominicana"}
 
-]
+# ]
 
 # def iterarDiccionario(lista):
 #     for diccionario in lista:
@@ -18,12 +18,30 @@ cantantes = [
 
 # IterarDiccionario(cantantes)
 
-def IterarDiccionario2(artista, nac):
-    for diccionario in nac:
-        if artista in diccionario:
-            print(diccionario[artista])
+# def IterarDiccionario2(artista, nac):
+#     for diccionario in nac:
+#         if artista in diccionario:
+#             print(diccionario[artista])
 
-# ejemplo de uso:
+# # ejemplo de uso:
 
-IterarDiccionario2("nombre", cantantes)
-IterarDiccionario2("pais", cantantes) 
+# IterarDiccionario2("nombre", cantantes)
+# IterarDiccionario2("pais", cantantes) 
+
+costa_rica = {
+
+   "ciudades": ["San José", "Limón", "Cartago", "Puntarenas"],
+
+   "comidas": ["gallo pinto", "casado", "tamales", "chifrijo", "olla de carne"]
+
+}
+
+def informacionDiccionario(diccionario):
+    for clave, valores in diccionario.items():
+        print(f"{len(valores)} {clave.upper()}")
+        for valor in valores:
+            print(valor)
+
+print("\n iterar a traves de un diccionario con valores de la lista")
+
+informacionDiccionario(costa_rica)
