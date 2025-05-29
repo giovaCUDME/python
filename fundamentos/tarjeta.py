28e15b534594d0e82fba1f75a70b20b9602f5a9f
giovaCUDME <giovanni.molinet.navarrete@alumnos.sip.cl>
2025-05-29 09:18:19 -0400

    commit

---
+++ fundamentos/tarjeta.py
@@ -0,0 +1,41 @@
+#giovanni molinet
+#29/05/2025
+
+class tarjetaCredito:
+    def __init__(self, saldo_pagar=0, limite_credito = 100000, interes = 0.02):
+        self.saldo_pagar = saldo_pagar
+        self.limite_credito = limite_credito
+        self.interes = interes
+
+    def comprar(self, monto):
+        if self.saldo_pagar+monto<=self.limite_credito:
+            self.saldo_pagar +=monto
+            print(f"Compra realizada: ${monto:,}.nuevo saldo:${self.saldo_pagar:,}")
+        else:
+            print("tarjeta rechaza, haz alncanzado tu limite de credito.+")
+            return self
+        
+    def pago(self, monto):
+        self.saldo_pagar -= monto
+        if self.saldo_pagar<0:
+            self.saldo =0
+            print(f"Pago realizado:${monto:,} nuevo saldo: ${self.saldo_pagar:,}")
+            return self #nos permite encadenar los metodos
+        
+    def mostrar_info_tarjeta(self):
+        print("----- Informacion de la tarjeta -----")
+        print(f"saldo a pagar:${self.saldo_pagar:,}")
+        print(f"Limite de credito:{self.limite_credito}")
+        print("------------------------------------")
+        return self
+    
+    def cobrar_interes(self):
+        self.saldo_pagar += self.saldo_pagar *self.interes
+        print(f"interes cobrado. Nuevo saldo:${self.saldo_pagar:,.2f}")
+        return self
+
+tarjeta1 = tarjetaCredito(limite_credito=200000, interes = 0.02)
+tarjeta1.comprar(50000)
+tarjeta1.pago(30000)
+tarjeta1.mostrar_info_tarjeta()
+tarjeta1.cobrar_interes()
