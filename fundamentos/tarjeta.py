9fd0d09b667c68f251c820dc3b4941e386dbf90c
giovaCUDME <giovanni.molinet.navarrete@alumnos.sip.cl>
2025-05-29 10:37:25 -0400

    commit

--- fundamentos/tarjeta.py
+++ fundamentos/tarjeta.py
@@ -1,11 +1,14 @@
 #giovanni molinet
 #29/05/2025
 
-class tarjetaCredito:
+class TarjetaCredito:
+    tarjetas = []
+    
     def __init__(self, saldo_pagar=0, limite_credito = 100000, interes = 0.02):
         self.saldo_pagar = saldo_pagar
         self.limite_credito = limite_credito
         self.interes = interes
+        TarjetaCredito.tarjetas.append(self)
 
     def comprar(self, monto):
         if self.saldo_pagar+monto<=self.limite_credito:
@@ -36,11 +39,29 @@ class tarjetaCredito:
     
     @classmethod
     def mostrar_todas_tarjetas(cls):
-        for i, tarjeta in enumerate (cls.tarjeta, 1)
-        print(f"tarjeta {i:} saldo:${tarjeta.saldo_pagar:,}, limite:${tarjeta.limite_credito:,}, interes:{tarjeta.interes:.2%}")
+        for i, tarjeta in enumerate (cls.tarjeta, 1):
+            print(f"tarjeta {i:} saldo:${tarjeta.saldo_pagar:,}, limite:${tarjeta.limite_credito:,}, interes:{tarjeta.interes:.2%}")
 
-tarjeta1 = tarjetaCredito(limite_credito=200000, interes = 0.02)
-tarjeta1.comprar(50000)
-tarjeta1.pago(30000)
-tarjeta1.mostrar_info_tarjeta()
-tarjeta1.cobrar_interes() 
+print("======= tarjeta 1 =======")
+tarjeta1 = TarjetaCredito()
+tarjeta1.comprar(50000).comprar(20000).pago(10000).cobrar_interes().mostrar_info_tarjeta
+
+print("=================================================")
+
+print("======= tarjeta 2 =======")
+tarjeta2 = TarjetaCredito()
+tarjeta2.comprar(70000).comprar(50000).pago(40000).cobrar_interes().mostrar_info_tarjeta
+
+print("=================================================")
+
+print("======= tarjeta 3 =======")
+tarjeta3 = TarjetaCredito()
+tarjeta3.comprar(30000).comprar(300000).cobrar_interes().mostrar_info_tarjeta
+
+print("=================================================")
+
+# tarjeta1 = tarjetaCredito(limite_credito=200000, interes = 0.02)
+# tarjeta1.comprar(50000)
+# tarjeta1.pago(30000)
+# tarjeta1.mostrar_info_tarjeta()
+# tarjeta1.cobrar_interes() 
---
+++ fundamentos/usuario.py
@@ -0,0 +1,12 @@
+#giovanni molinet
+
+class Usuario:
+    def __init__(self, nombre, apellido, email):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.limite_credito = 30000
+        self.saldo_pagar = 0
+
+    def hacer_compra(aself, monto, indice_tarjeta=0):
+        """"""
